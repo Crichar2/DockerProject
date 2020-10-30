5adc65865f09fce70ff31e90ea985d4378eb061b
Stormeru <c.richard@etud.univ-pau.fr>
2020-10-30 16:02:31 +0100

    Version finale

--- Projet/dbinit/dbinit.sql
+++
@@ -1,4 +0,0 @@
-DROP DATABASE IF EXISTS dockerdemo;
-CREATE DATABASE dockerdemo;
-GRANT ALL PRIVILEGES ON dockerdemo.* TO 'demo'@'%' IDENTIFIED BY 'demo';
-GRANT ALL PRIVILEGES ON dockerdemo.* TO 'demo'@'localhost' IDENTIFIED BY 'demo';
